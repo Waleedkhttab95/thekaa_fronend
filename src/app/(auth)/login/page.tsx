0fd1b9115eb70838900d0a1080e66b06386035cd
anas-alnabahin <anasnb2002nb@gmail.com>
2025-02-26 11:40:30 +0200

    creating auth button component

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,11 +1,12 @@
-import { Button } from "@/components/atoms/button";
-import GoogleLoginButton from "@/components/atoms/googleLoginButton";
+import AuthButton from "@/components/atoms/AuthButton";
 
 const LoginPage = () => {
   return (
-    <div className="flex">
-      <Button className="md:w-[487px] h-[56px] text-base">تسجيل الدخول</Button>
-      <GoogleLoginButton />
+    <div className="flex flex-col">
+      <AuthButton authButtonType="basic">
+        تسجيل الدخول
+      </AuthButton>
+      <AuthButton authButtonType="google" />
     </div>
   );
 };
