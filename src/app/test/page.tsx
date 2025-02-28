c2dc9f2284419628079864b42f5bc74f01c722c0
MohammedAl-Nabahin <moha.nabahin@gmail.com>
2025-02-28 16:25:43 +0200

    Update test page

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,61 +1,108 @@
-'use client';
+"use client";
 
+import { useState } from "react";
+import mockQuestions from "./mockQuestions.json";
 import { Button } from "@/components/atoms/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/molecules/card";
+import { Question } from "@/types/question.types";
+import { Questions } from "@/components/organisms/Questions";
+import { ExamCompletion } from "@/components/molecules/ExamCompletion";
+import arrow from "../../../public/arrow.svg";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/molecules/card";
+import Image from "next/image";
 
 export default function TestPage() {
+  const questions = mockQuestions as Question[];
+  const [answer, setAnswer] = useState("");
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [selectedAnswers, setSelectedAnswers] = useState<
+    Record<string, string>
+  >({});
+  const [isCompleted, setIsCompleted] = useState(false);
+
+  if (questions.length === 0) {
+    return <p className="text-center mt-10 text-red-500">Error Loading...</p>;
+  }
+
+  const currentQuestion = questions[currentQuestionIndex] as Question;
+  const isLastQuestion = currentQuestionIndex === questions.length - 1;
+
+  const handleAnswerSelect = (answerId: string) => {
+    setSelectedAnswers((prev) => ({
+      ...prev,
+      [currentQuestion.id]: answerId,
+    }));
+  };
+
+  const handleNext = () => {
+    if (currentQuestion.type === "fill" && !answer.trim()) return;
+    if (!selectedAnswers[currentQuestion.id] && currentQuestion.type !== "fill")
+      return;
+
+    if (isLastQuestion) {
+      setIsCompleted(true);
+      return;
+    }
+
+    setCurrentQuestionIndex((prev) => prev + 1);
+    setAnswer("");
+  };
 
   return (
-    <div className="min-h-screen p-4 bg-yellow-100">
+    <div className="p-4 bg-blue-300">
       <div className="container mx-auto flex justify-center items-center min-h-screen">
-            <Card variant="default" className="w-full max-w-2xl pt-4">
-
-                <CardHeader>
-                    <CardDescription>سؤال 1 من 5</CardDescription>
-                    <CardTitle>اختبار تحديد المستوى</CardTitle>
-                </CardHeader>
-
-                <CardContent>
-                    <div className="space-y-6">
-                        <div className="text-right">
-                        <h2 className="text-lg font-tajawal font-medium mb-4">ما هي المادة التي لا تختلط مع المياه؟</h2>
-                            <div className="space-y-3 p-2">
-                                <div className="flex items-center justify-end gap-2 p-3 rounded-lg border hover:bg-gray-50 cursor-pointer">
-                                    <label className="w-full text-right cursor-pointer font-ibm font-regular">الزيت</label>
-                                    <input type="radio" name="answer" className="w-4 h-4" />
-                                </div>
-                            </div>
-                            <div className="space-y-3 p-2">
-                                <div className="flex items-center justify-end gap-2 p-3 rounded-lg border hover:bg-gray-50 cursor-pointer">
-                                    <label className="w-full text-right cursor-pointer font-ibm font-regular">السكر</label>
-                                    <input type="radio" name="answer" className="w-4 h-4" />
-                                </div>
-                            </div> 
-                            <div className="space-y-3 p-2">
-                                <div className="flex items-center justify-end gap-2 p-3 rounded-lg border hover:bg-gray-50 cursor-pointer">
-                                    <label className="w-full text-right cursor-pointer font-ibm font-regular">الملح</label>
-                                    <input type="radio" name="answer" className="w-4 h-4" />
-                                </div>
-                            </div> 
-                            <div className="space-y-3 p-2">
-                                <div className="flex items-center justify-end gap-2 p-3 rounded-lg border hover:bg-gray-50 cursor-pointer">
-                                    <label className="w-full text-right cursor-pointer font-ibm font-regular">الزيت</label>
-                                    <input type="radio" name="answer" className="w-4 h-4" />
-                                </div>
-                            </div>  
-                        </div>
-                    </div>    
-                </CardContent>
-
-                <CardFooter>
-                        <Button 
-                            variant="default"
-                            className="w-full max-w-full sm:w-auto bg-gray-900 hover:bg-gray-800 text-white px-4 sm:px-8 py-2 font-pingar font-medium text-sm sm:text-base"
-                        >
-                            التالي ← 
-                        </Button>
-                </CardFooter>
-            </Card>
+        {isCompleted ? (
+          <ExamCompletion />
+        ) : (
+          <Card
+            variant="default"
+            className="w-[1141px] max-h-[770] max-w-full p-8"
+          >
+            <CardHeader>
+              <CardDescription>
+                سؤال {currentQuestionIndex + 1} من {questions.length}
+              </CardDescription>
+              <CardTitle className="font-pingar font-bold text-2xl">
+                إختبار تحديد المستوى
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="flex justify-center">
+              <Questions
+                question={currentQuestion}
+                selectedAnswer={selectedAnswers[currentQuestion.id]}
+                onSelectAnswer={handleAnswerSelect}
+                answer={answer}
+                setAnswer={setAnswer}
+              />
+            </CardContent>
+            <CardFooter className="flex justify-end">
+              <Button
+                onClick={handleNext}
+                disabled={
+                  currentQuestion.type === "fill"
+                    ? !answer.trim()
+                    : !selectedAnswers[currentQuestion.id]
+                }
+                className="text-[16px] font-pingar font-bold w-[193px] h-[56px] flex flex-row justify-center items-center text-start"
+              >
+                {isLastQuestion ? "إنهاء الإختبار" : "التالي"}
+                <Image
+                  src={arrow}
+                  alt="arrow"
+                  width={17.5}
+                  height={11.5}
+                  color="white"
+                ></Image>
+              </Button>
+            </CardFooter>
+          </Card>
+        )}
       </div>
     </div>
   );
