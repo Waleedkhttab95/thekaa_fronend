6c1cf29307de9b60cdfdf9171d6d1a7daee3dc11
se-bayan-elian <bezo2024@gmail.com>
2025-03-01 14:09:17 +0200

    fix: 2 scrolls appears & fixed images invisible

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -3,11 +3,11 @@ import Header from '../organisms/Header'
 import Image from 'next/image'
 const MainLayout = ({ children }: PropsWithChildren) => {
   return (
-    <div className='relative w-100vw min-h-[100vh] overflow-x-hidden'>
+    <div className='main-layout relative w-100vw min-h-[100vh] z-[-1]'>
       <Header />
       {children}
-      <Image className='absolute top-[-121px] z-[-1] left-[-100px]' src="/assets/images/top-left.svg" alt="acc" width={404} height={377} />
-      <Image className='absolute bottom-[-80px] z-[-1] right-[-20px]' src="/assets/images/bottom-right.svg" alt="acc" width={404} height={377} />
+      <Image className='fixed top-[-121px] z-[-1] left-[-100px]' src="/assets/images/top-left.svg" alt="acc" width={404} height={377} />
+      <Image className='fixed bottom-[-80px] z-[-1] right-[-20px]' src="/assets/images/bottom-right.svg" alt="acc" width={404} height={377} />
     </div>
   )
 }
