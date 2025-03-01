27d01910ea73e839c7f877f28f4a0dca3c7a1434
Bayan Elian Alreqeb <104948334+se-bayan-elian@users.noreply.github.com>
2025-03-01 14:28:07 +0200

    fix/ element z-indecies

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -3,11 +3,11 @@ import Header from '../organisms/Header'
 import Image from 'next/image'
 const MainLayout = ({ children }: PropsWithChildren) => {
   return (
-    <div className='main-layout relative w-100vw min-h-[100vh] z-[-1]'>
+    <div className='main-layout relative w-100vw min-h-[100vh]'>
       <Header />
-      {children}
-      <Image className='fixed top-[-121px] z-[-1] left-[-100px]' src="/assets/images/top-left.svg" alt="acc" width={404} height={377} />
-      <Image className='fixed bottom-[-80px] z-[-1] right-[-20px]' src="/assets/images/bottom-right.svg" alt="acc" width={404} height={377} />
+      <div className='z-[1]' >{children}</div>
+      <Image className='fixed top-[-121px]  left-[-100px]' src="/assets/images/top-left.svg" alt="acc" width={404} height={377} />
+      <Image className='fixed bottom-[-80px] right-[-20px]' src="/assets/images/bottom-right.svg" alt="acc" width={404} height={377} />
     </div>
   )
 }
