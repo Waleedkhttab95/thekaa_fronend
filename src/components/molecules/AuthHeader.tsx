1bb20d3c892ef7f3b0f50e97006f1559dfdc0d73
anas-alnabahin <anasnb2002nb@gmail.com>
2025-03-04 12:58:15 +0200

    modify the verify account header to use in other components

---
+++ src/components/molecules/AuthHeader.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import { CardDescription, CardTitle } from "./card";
+
+// Define prop types
+interface AuthHeaderProps {
+  imageSrc: string;
+  imageAlt: string;
+  imageW: number;
+  imageH: number;
+  title: string;
+  subtitle: string;
+  subtitle2?: string;
+}
+
+const AuthHeader: React.FC<AuthHeaderProps> = ({
+  imageSrc,
+  imageAlt,
+  imageW,
+  imageH,
+  title,
+  subtitle,
+  subtitle2,
+}) => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <Image
+        src={imageSrc}
+        alt={imageAlt}
+        width={imageW}
+        height={imageH}
+        className="mb-3"
+      />
+      <div className="flex flex-col items-center gap-3">
+        <CardTitle>{title}</CardTitle>
+        <CardDescription className="text-forcedGray text-lg">
+          {subtitle}
+        </CardDescription>
+        {subtitle2 && (
+          <CardDescription className="text-forcedGray text-lg">
+            {subtitle2}
+          </CardDescription>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AuthHeader;
--- src/components/pages/VerifyAccountPage.tsx
+++ src/components/pages/VerifyAccountPage.tsx
@@ -1,10 +1,26 @@
-import VerifyAccountHeader from "../atoms/VerifyAccountHeader";
+import VerifyAccountHeader from "../molecules/AuthHeader";
 import AuthLayout from "../layouts/AuthLayout";
 import VerifyAccountForm from "../organisms/VerifyAccountForm";
 
+import AuthorizedLogo from "../../../public/authorized.svg";
+import { useTranslations } from "next-intl";
+
 const VerifyAccountPage = () => {
+  const t = useTranslations("VerifyAccountPage");
   return (
-    <AuthLayout headerTitle={<VerifyAccountHeader />}>
+    <AuthLayout
+      headerTitle={
+        <VerifyAccountHeader
+          imageSrc={AuthorizedLogo}
+          imageAlt="authorized"
+          imageW={53}
+          imageH={48}
+          title={t("title")}
+          subtitle={t("subTitle")}
+          subtitle2={t("subSubTitle")}
+        />
+      }
+    >
       <VerifyAccountForm />
     </AuthLayout>
   );
