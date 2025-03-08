bf8433031694c552293c4a89f447f95795d3b212
se-bayan-elian <bezo2024@gmail.com>
2025-03-08 12:39:24 +0200

    add: title meta data

--- src/app/sons-files/page.tsx
+++ src/app/sons-files/page.tsx
@@ -1,6 +1,16 @@
 import SonsStudentsManagementPage from '@/components/pages/SonsStudentsManagementPage'
+import { getLocale } from 'next-intl/server';
 import React from 'react'
-
+export async function generateMetadata() {
+  const locale = await getLocale() as keyof typeof metadataTranslations;
+  const metadataTranslations = {
+    en: {
+      title: 'Sons Files Management',
+    },
+    ar: { title: "إدارة ملفات الأبناء" },
+  };
+  return metadataTranslations[locale]
+}
 const SonsStudentsManagement = () => {
   return (
     <SonsStudentsManagementPage />
