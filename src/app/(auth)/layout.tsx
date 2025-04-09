0e7802b6e822db54cdf220b7802752ebec43c249
anas-alnabahin <anasnb2002nb@gmail.com>
2025-04-09 11:38:07 +0200

    implementing protect routes

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,30 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@/context/AuthContext";
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div className="flex justify-center items-center">
-      {children}
-    </div>
-  );
+  const { user, isLoading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isLoading && user) {
+      router.replace("/"); //
+    }
+  }, [isLoading, user, router]);
+
+  if (isLoading) {
+    return <div className="p-4 text-center">Loading...</div>;
+  }
+
+  if (user) {
+    return null;
+  }
+
+  return <div className="flex justify-center items-center">{children}</div>;
 }
