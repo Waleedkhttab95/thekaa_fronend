7ab2bf19c053d771f38e03a6be0cb3c7fef132c1
anas-alnabahin <anasnb2002nb@gmail.com>
2025-04-16 13:25:31 +0300

    displaying login error in a toast and fixing some fatel buggs

--- src/hooks/useLogin.tsx
+++ src/hooks/useLogin.tsx
@@ -1,9 +1,13 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginService } from "@/services/auth";
 import { axiosAuthClient } from "@/lib/axios";
+import { toast } from "@/components/atoms/sooner";
+import { useTranslations } from "next-intl";
 
 export const useLogin = () => {
   const queryClient = useQueryClient();
+
+  const t = useTranslations("LoginPage");
 
   return useMutation({
     mutationFn: (credentials: { email: string; password: string }) =>
@@ -12,11 +16,12 @@ export const useLogin = () => {
       console.log("Logging in with:", variables);
       await queryClient.cancelQueries({ queryKey: ["auth", "user"] });
     },
-    onError: (error) => {
-      console.error("Login error:", error);
-    },
-    onSettled: () => {
-      queryClient.invalidateQueries({ queryKey: ["auth", "user"] });
+    onError: () => {
+      toast({
+        title: t("loginError"),
+        description: t("loginErrorDescription"),
+        variant: "destructive",
+      });
     },
   });
 };
