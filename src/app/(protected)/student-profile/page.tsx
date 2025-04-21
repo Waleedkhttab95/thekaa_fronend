e52e0e58ca092e87dd1fd697f1b4440472267df8
anas-alnabahin <anasnb2002nb@gmail.com>
2025-04-21 14:59:46 +0300

    fixing the footer in dashboard layout

--- src/app/(protected)/student-profile/page.tsx
+++ src/app/(protected)/student-profile/page.tsx
@@ -1,4 +1,18 @@
+import { getLocale } from "next-intl/server";
+import StudentProfilePage from "@/components/pages/StudentProfilePage";
+
+export async function generateMetadata() {
+  const locale = (await getLocale()) as keyof typeof metadataTranslations;
+  const metadataTranslations = {
+    en: {
+      title: "Student Profile",
+    },
+    ar: { title: "صفحة الطالب" },
+  };
+  return metadataTranslations[locale];
+}
+
 const StudentProfile = () => {
-  return <div>StudentProfile</div>;
+  return <StudentProfilePage />;
 };
 export default StudentProfile;
