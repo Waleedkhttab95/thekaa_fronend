4df8f743529bfdf830a6e649754a20223a40b003
anas-alnabahin <anasnb2002nb@gmail.com>
2025-04-21 15:10:56 +0300

    fixing dashboard layout

--- src/components/pages/StudentProfilePage.tsx
+++ src/components/pages/StudentProfilePage.tsx
@@ -1,8 +1,6 @@
 const StudentProfilePage = () => {
   return (
-    <div className="bg-[url('/student-profile-bg.png')] bg-cover rounded-[40px] h-[744px] w-10/12 mt-16 mb-36">
-      
-    </div>
+    <div className="bg-[url('/student-profile-bg.png')] bg-cover rounded-[40px] h-[744px] w-10/12"></div>
   );
 };
 export default StudentProfilePage;
