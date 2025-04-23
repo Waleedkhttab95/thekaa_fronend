97db424b7797e105d863e809ffb9add201424d8f
Mohammedal-nabahin <moha.nabahin@gmail.com>
2025-04-23 16:38:49 +0300

    Update questions components

--- src/components/molecules/card.tsx
+++ src/components/molecules/card.tsx
@@ -68,7 +68,10 @@ const CardContent = React.forwardRef<HTMLDivElement, CardComponentProps>(
   ({ className, variant, ...props }, ref) => (
     <div
       ref={ref}
-      className={cn("p-6 pt-0 w-full flex justify-center items-center", className)}
+      className={cn(
+        "p-6 pt-0 w-full flex justify-center items-center",
+        className
+      )}
       {...props}
     />
   )
