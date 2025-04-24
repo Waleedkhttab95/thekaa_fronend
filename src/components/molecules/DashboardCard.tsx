a2860dffad179addb99870f55759b49d38d9ba65
anas-alnabahin <anasnb2002nb@gmail.com>
2025-04-24 10:30:30 +0300

    finishing plan page

--- src/components/molecules/DashboardCard.tsx
+++ src/components/molecules/DashboardCard.tsx
@@ -9,10 +9,10 @@ const DashboardCard = ({
   cardTitle: string;
 }) => {
   return (
-    <div className="w-full min-w-[325px] min-[1039px]:max-w-[375px]">
-      <Card className="h-48 bg-white/60 shadow">
-        <CardHeader className="flex items-center justify-center text-2xl font-bold text-center">
-          {cardTitle}
+    <div className="w-full max-w-[80%] md:max-w-[280px] xl:max-w-[30%]  aspect-[65/32] cursor-pointer hover:opacity-70 overflow-hidden">
+      <Card className="h-full bg-white/60 shadow flex flex-col justify-center md:justify-evenly">
+        <CardHeader className="flex items-center justify-center text-xl sm:text-3xl md:text-sm xl:text-lg font-bold text-center">
+          <p>{cardTitle}</p>
         </CardHeader>
         <CardContent className="">{children}</CardContent>
       </Card>
