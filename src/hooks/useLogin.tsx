b90cc8c1961a8c78688c4ebf6565aeb22acf5c5f
anas-alnabahin <anasnb2002nb@gmail.com>
2025-04-24 21:14:40 +0300

    using normal client for login and register

--- src/hooks/useLogin.tsx
+++ src/hooks/useLogin.tsx
@@ -1,6 +1,6 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { getUser, login as loginService } from "@/services/auth";
-import { axiosAuthClient } from "@/lib/axios";
+import { axiosClient } from "@/lib/axios";
 import { toast } from "@/components/atoms/sooner";
 import { useTranslations } from "next-intl";
 import { useRouter } from "next/navigation";
@@ -15,7 +15,7 @@ export const useLogin = () => {
 
   return useMutation({
     mutationFn: (credentials: { email: string; password: string }) =>
-      loginService(axiosAuthClient, credentials),
+      loginService(axiosClient, credentials),
     onMutate: async (variables) => {
       console.log("Logging in with:", variables);
       await queryClient.cancelQueries({ queryKey: ["auth", "user"] });
