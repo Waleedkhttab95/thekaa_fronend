e0f2183945c60a4302f3907eccaa674fdc523f51
anas-alnabahin <anasnb2002nb@gmail.com>
2025-04-24 15:59:24 +0300

    adding interceptor to add access token to requests

--- src/hooks/useAxiosAuth.ts
+++ src/hooks/useAxiosAuth.ts
@@ -4,24 +4,28 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { deleteCookie, getCookie } from "cookies-next/client";
 import { logout } from "@/services/auth";
+
 export const useAxiosAuth = () => {
   const router = useRouter();
-  const accessToken = getCookie("token");
+
   useEffect(() => {
     const reqIntercept = axiosAuthClient.interceptors.request.use(
       (config) => {
-        // if (!config.headers["Authorization"])
-        //   // config.headers["Authorization"] = `Bearer ${accessToken}`;
-
+        const accessToken = getCookie("Authentication");
+        if (accessToken) {
+          config.headers["Authorization"] = `Bearer ${accessToken}`;
+        }
         return config;
       },
       (error) => Promise.reject(error)
     );
+
     const resIntercept = axiosAuthClient.interceptors.response.use(
       (response) => response,
       async (error) => {
         const prevReq = error.config;
         const isLogoutRequest = prevReq.url?.includes("/auth/logout");
+
         if (
           error.response &&
           error.response.status === 401 &&
@@ -31,22 +35,24 @@ export const useAxiosAuth = () => {
           prevReq.sent = true;
           try {
             await logout(axiosAuthClient);
-            deleteCookie("token");
+            deleteCookie("Authentication");
             deleteCookie("role");
-            // redirect to login page
             router.replace("/login");
           } catch (error: any) {
             return Promise.reject(error);
           }
           return axiosAuthClient(prevReq);
         }
+
         return Promise.reject(error);
       }
     );
+
     return () => {
       axiosAuthClient.interceptors.request.eject(reqIntercept);
       axiosAuthClient.interceptors.response.eject(resIntercept);
     };
-  }, [accessToken]);
+  }, []);
+
   return axiosAuthClient;
 };
