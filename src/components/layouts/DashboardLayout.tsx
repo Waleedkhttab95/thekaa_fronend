ebc27bfade073c393c229d54725c47f24435b9fe
anas-alnabahin <anasnb2002nb@gmail.com>
2025-04-29 12:41:22 +0300

    adding some links in the navBar for the dashboard

--- src/components/layouts/DashboardLayout.tsx
+++ src/components/layouts/DashboardLayout.tsx
@@ -7,6 +7,8 @@ import LocaleSwitcher from "../molecules/LocaleSwitcher";
 import ProfileLogo from "../molecules/ProfileLogo";
 import { Button } from "../atoms/button";
 import { useProfile } from "@/hooks/rqs/profile";
+import Link from "next/link";
+import { ProtectedRoutes } from "@/config/routes";
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
   const locale = useLocale();
@@ -18,35 +20,39 @@ const DashboardLayout = ({ children }: { children: ReactNode }) => {
         className="bg-[linear-gradient(to_right,_#23F6F04D_0%,_#CBBDF24D_50%,_#F7AEF34D_100%)] 
         w-11/12 sm:w-10/12 rounded-[40px] h-20 px-8 max-w-[1300px] min-w-[360px] flex justify-between"
       >
-        {locale === Locales.ar ? (
-          <Image
-            className="hidden sm:block"
-            src={"/assets/images/ar-logo.svg"}
-            alt="logo"
-            width={94}
-            height={26}
-          />
-        ) : (
-          <Image
-            className="hidden sm:block"
-            src={"/assets/images/en-logo.svg"}
-            alt="logo"
-            width={94}
-            height={26}
-          />
-        )}
+        <Link href={"/"} className="flex items-center justify-center">
+          {locale === Locales.ar ? (
+            <Image
+              className="hidden sm:block"
+              src={"/assets/images/ar-logo.svg"}
+              alt="logo"
+              width={94}
+              height={26}
+            />
+          ) : (
+            <Image
+              className="hidden sm:block"
+              src={"/assets/images/en-logo.svg"}
+              alt="logo"
+              width={94}
+              height={26}
+            />
+          )}
+        </Link>
         <div className="flex items-center gap-3 justify-evenly sm:justify-start w-full sm:w-auto">
           <LocaleSwitcher onlySmall />
           <ProfileLogo name={profile?.parentName?.split(" ")![0] ?? "??"} />
-          <Button className="sm:w-48 text-base">
-            <Image
-              src={"/dashboard-circle.svg"}
-              alt="dashboard"
-              width={24}
-              height={24}
-            />
-            <p className="hidden sm:block">{t("dashboard")}</p>
-          </Button>
+          <Link href={ProtectedRoutes.Dashboard}>
+            <Button className="sm:w-48 text-base">
+              <Image
+                src={"/dashboard-circle.svg"}
+                alt="dashboard"
+                width={24}
+                height={24}
+              />
+              <p className="hidden sm:block">{t("dashboard")}</p>
+            </Button>
+          </Link>
         </div>
       </div>
       <div className="mt-16 mb-36 w-11/12 sm:w-10/12 max-w-[1300px] min-w-[360px] min-h-[calc(100vh-352px)]">
