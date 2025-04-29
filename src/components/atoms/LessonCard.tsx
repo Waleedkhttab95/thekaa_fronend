77b8b5cb728ff4b75e1868eb5c88a8fdd7ed091e
anas-alnabahin <anasnb2002nb@gmail.com>
2025-04-29 12:29:56 +0300

    fixing animations

--- src/components/atoms/LessonCard.tsx
+++ src/components/atoms/LessonCard.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { cn } from "@/lib/utils";
 import { motion } from "framer-motion";
 
@@ -13,27 +14,23 @@ const LessonCard = ({
 }) => {
   return (
     <motion.div
-      initial={{ opacity: 0, y: 20 }}
+      initial={{ opacity: 0, y: -20 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 2, ease: "easeOut" }}
-      whileHover={{
-        scale: 1.02,
-        boxShadow: "0px 6px 20px rgba(0,0,0,0.08)",
-        transition: { type: "spring", stiffness: 250, damping: 15 },
-      }}
+      transition={{ duration: 0.6, ease: "easeOut" }}
+      whileHover={{ scale: 1.03 }}
       className={cn(
         "flex items-center text-xs bg-white/60 w-[250px] sm:w-full h-[50px] font-bold border border-white rounded-[40px] ps-1 text-[#6C6063] cursor-pointer z-20",
         firstLesson && "text-black"
       )}
     >
-      <p className="w-[80px] text-center">{date}</p>
+      <p className="w-[100px] text-center">{date}</p>
       <span
         className={cn(
           "w-[1px] h-6 bg-[#6C6063] mx-4",
           firstLesson && "bg-black"
         )}
       ></span>
-      <p className="flex-1">{lesson}</p>
+      <p className="flex-1 pe-2">{lesson}</p>
     </motion.div>
   );
 };
