2b0df47be81c8d79c58601b5d7663a2c2ca5b826
MohammedAl-Nabahin <moha.nabahin@gmail.com>
2025-05-12 17:31:44 +0300

    Update messages and student types

--- src/types/student.type.ts
+++ src/types/student.type.ts
@@ -14,54 +14,16 @@ export interface IStudentData {
   parentId: string;
 }
 
-export interface IEducationPlan {
-  _id: string;
-  isDeleted: boolean;
-  createdAt: string;
-  updatedAt: string;
-  subjectId: string;
-  level: string;
-  startDate: string;
-  endDate: string;
-  status: string;
-  userId: string;
-  studentId: string;
-  currentLesson: {
-    lessonId: string;
-    lessonName: string;
-    unitName: string;
-    unitNumber: number;
-  };
-  nextLesson: {
-    lessonId: string;
-    lessonName: string;
-    unitName: string;
-    unitNumber: number;
-  };
-  lastLesson: {
-    lessonId: string;
-    lessonName: string;
-    unitName: string;
-    unitNumber: number;
-  };
-  numberOfLessons: number;
-  completedLessons: [];
-  classPerWeek: number;
-  listOfLessons: {
-    lessonId: string;
-    lessonName: string;
-    unitName: string;
-    unitNumber: number;
-  }[];
-  quizzes: [];
-  totalQuizzes: number;
-  nextQuiz: string;
-}
-
 export interface INextLesson {
   lessonId: string;
   lessonName: string;
   unitName: string;
   unitNumber: number;
   date: string;
+}
+
+export interface IStudentProgress {
+  planPrecentage: number;
+  quizDay: boolean;
+  lessonDay: boolean;
 }
